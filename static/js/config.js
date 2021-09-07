846823a5a5be27f6b2251af6eff7ff8f62bd29e8
melcardenas28 <83568152+melcardenas28@users.noreply.github.com>
2021-09-06 22:25:21 -0700

    update

---
+++ static/js/config.js
@@ -0,0 +1,4 @@
+// API key
+const API_KEY = "pk.eyJ1IjoidmVlbmFjdiIsImEiOiJjazR6eGJ5ZWIwZjdmM210ZTgya3lwNzJiIn0.0qK5yFQHuncyG9NxIaUKQw";
+const MAPBOX_URL = "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}";
+const ATTRIBUTION = "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>";
